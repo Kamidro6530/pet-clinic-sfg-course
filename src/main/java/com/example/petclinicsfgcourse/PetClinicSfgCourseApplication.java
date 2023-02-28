c3d8190da0ba738f9a951263ff60f75daba87c77
Kamidro6530 <daniel6530@wp.pl>
2023-02-28 14:15:59 +0100

    Init Project add

---
+++ src/main/java/com/example/petclinicsfgcourse/PetClinicSfgCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicsfgcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSfgCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicSfgCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petclinicsfgcourse/PetClinicSfgCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicsfgcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSfgCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
