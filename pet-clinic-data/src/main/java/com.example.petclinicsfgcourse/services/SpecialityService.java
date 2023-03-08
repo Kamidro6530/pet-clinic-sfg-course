d6013fbf09555503601122a280f83a0c2c2acbb2
Kamidro6530 <daniel6530@wp.pl>
2023-03-08 08:44:30 +0100

    Created SpecialityService and SpecialityMapService. Closes #31

---
+++ pet-clinic-data/src/main/java/com.example.petclinicsfgcourse/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.example.petclinicsfgcourse.services;
+
+import com.example.petclinicsfgcourse.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
