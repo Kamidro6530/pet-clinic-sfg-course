1fd4619392f0b7ed44ca090086693588bd0a63d2
Kamidro6530 <daniel6530@wp.pl>
2023-03-09 10:53:05 +0100

    Convert Owners to JPA Entities. Closes #36

--- pet-clinic-data/src/main/java/com.example.petclinicsfgcourse/model/Person.java
+++ pet-clinic-data/src/main/java/com.example.petclinicsfgcourse/model/Person.java
@@ -1,9 +1,16 @@
 package com.example.petclinicsfgcourse.model;
 
-public class Person extends BaseEntity{
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 
-  private String firstName;
-  private String lastName;
+@MappedSuperclass
+public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     public String getFirstName() {
         return firstName;
--- pet-clinic-data/src/main/java/com.example.petclinicsfgcourse/model/PetType.java
+++ pet-clinic-data/src/main/java/com.example.petclinicsfgcourse/model/PetType.java
@@ -1,6 +1,15 @@
 package com.example.petclinicsfgcourse.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+    @Column(name = "name")
+    private String name;
+
     public String getName() {
         return name;
     }
@@ -8,6 +17,4 @@ public class PetType extends BaseEntity{
     public void setName(String name) {
         this.name = name;
     }
-
-    private String name;
 }
